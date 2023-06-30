6914f0bd84f0bad4af99845b6eddbd795f296722
kotegg <lolpol.kek123@gmail.com>
2023-06-30 23:09:29 +0300

    3

--- src/main/java/school/mjc/stage0/module3/task2/AdvancedNamingConvention.java
+++ src/main/java/school/mjc/stage0/module3/task2/AdvancedNamingConvention.java
@@ -6,13 +6,13 @@ public class AdvancedNamingConvention {
     private int age;
     private int phoneNumber;
 
-    void callToFriend() {
-        //some code
-    }
-
-    void callByNumber(int number) {
-        //some code
-    }
+//    void callToFriend() {
+//        //some code
+//    }
+//
+//    void callByNumber(int number) {
+//        //some code
+//    }
 
     public static void main(String[] args) {
 
